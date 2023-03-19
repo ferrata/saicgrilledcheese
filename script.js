e78caf4f3526fdcd323d82dcd9a1f22d9e0d4988
Sasha Goloshchapov <ferrata@hey.com>
2023-03-19 04:18:38 -0500

    script.js: reimplemented drag-n-drop and checkbox matching. the idea is each image (draggable) can be placed into a container (drop zone). once drop is done, if the image is dropped into right place, then it finds and updates the corresponding checkbox.

--- script.js
+++ script.js
@@ -1,126 +1,82 @@
 const draggables = document.querySelectorAll('.draggable')
 const containers  =  document.querySelectorAll('.container')
 
-draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
-        draggable.classList.add('dragging')
-    })
-
-    draggable.addEventListener('dragend', () => {
-        draggable.classList.remove('dragging')
-    })
-
-containers.forEach(container => {
-    container.addEventListener('dragover', e => {
-    console.log('drag over');
-    e.preventDefault()
-    const afterElement = getDragAfterElement(container, e.clientY);
-    const draggable = document.querySelector('.dragging');
-    container.appendChild(draggable);
-    })
-})
-
-})
-
-function getDragAfterElement(container, y) {
-   const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
-
-   return draggableElements.reduce((closest, child) => {
-        const box = child.getBoundingClientRect()
-        const offset = y - box.top - box.height / 2
-        console.log(box)
-        console.log(offset)
-
-        if (offset < 0 && offset> closest.offset) {
-            return { offset: offset, element: child } 
-        } else {
-               return closest
-            }
-       
-
-   }, {offset: Number.NEGATIVE_INFINITY}).element
+function getFileNameFromUrl(url) {
+  // return a file name from URL, e.g. http://127.0.0.1:8080/images/01b_pan_on.png -> 01b_pan_on.png
+  const elements = url.split("/");
+  return elements[elements.length - 1];
 }
 
-function uncheckAll() {
-    document.querySelectorAll('input[type="checkbox"]')
-      .forEach(el => el.checked = false);
-  }
-  
-  document.querySelector('button').addEventListener('click', uncheckAll)
+function getStepFromFileName(fileName) {
+  // return a step name from a file name, e.g. 01b_pan_on.png -> 01b
+  return fileName.split("_")[0];
+}
 
-  const pictures = document.querySelectorAll('.picture');
-  pictures.forEach(picture => {
-    picture.addEventListener('drag', checkOrder);
-    picture.addEventListener('dragstart', (e) => {
-      e.dataTransfer.setData('text/plain', index);
-    } )
-  
+function getImagesOrder() {
+  // go through all <img> tags on the page and collect corresponsing step names
+  const order = [];
+  document.querySelectorAll("img").forEach((element) => {
+    const fileName = getFileNameFromUrl(element.src);
+    order.push(fileName);
+  });
+  return order;
+}
+
+function refreshCheckboxes() {
+  // get images order and the checklist order
+  // (we are assuming that the checklist order is the correct order)
+  const images = getImagesOrder();
+  const checklistItems = document.querySelectorAll(".cbox");
+
+  // compare image on each position with a corresponding checkbox, 
+  // update each checkbox with checked true/false
+  images.forEach((imageName, index) => {
+    const imageStep = getStepFromFileName(imageName);
+    const shouldBeChecked = imageStep === checklistItems[index].id;
+    checklistItems[index].checked = shouldBeChecked;
+  });
+}
+
+draggables.forEach((draggable) => {
+  draggable.addEventListener("dragstart", () => {
+    draggable.classList.add("dragging");
   });
 
-  // Get all the images and the checklist
-const images = document.querySelectorAll('.image-container img');
-const checklistItems = document.querySelectorAll('.checklist li');
-
-// Add drag and drop functionality to each image
-images.forEach((image) => {
-  // Set the data that will be transferred during the drag
-  image.addEventListener('dragstart', (event) => {
-    event.dataTransfer.setData('text/plain', event.target.id);
-  });
-  
-  // Prevent default behavior for dragover and drop events
-  image.addEventListener('dragover', (event) => {
-    event.preventDefault();
-  });
-  
-  image.addEventListener('drop', (event) => {
-    event.preventDefault();
-    const id = event.dataTransfer.getData('text/plain');
-    const draggableElement = document.getElementById(id);
-    const dropzone = event.target.closest('.image-container');
-    const checklistItem = checklistItems[id.charAt(id.length-1)-1];
-    
-    // Check if the image is being dropped in the appropriate location
-    if (dropzone === image.parentElement) {
-      dropzone.classList.add('dropzone');
-      dropzone.appendChild(draggableElement);
-      checklistItem.style.color = 'green';
-      checklistItem.innerHTML += ' &#10004;';
-    } else {
-      alert('Wrong location!');
-    }
+  draggable.addEventListener("dragend", () => {
+    // when drag is complete, refresh all the checkboxes
+    draggable.classList.remove("dragging");
+    refreshCheckboxes();
   });
 });
 
-const correctOrder = ['<img src="./images/01a_pan_off.png" class="draggable" draggable="true" height="300" width="300">', '<img src="./images/01b_pan_on.png" class="draggable" draggable="true" height="300" width="300">', '<img src="./images/02_butter_bread.png" class="draggable" draggable="true" height="300" width="300">',  '<img src="./images/03a_bread_in_pan.png" class="draggable" draggable="true" height="300" width="300">', '<img src="./images/04_buttered_bread_on_top.png" class="draggable" draggable="true" height="300" width="300">', '<img src="./images/05_flip.png" class="draggable" draggable="true" height="300" width="300">'];
+containers.forEach((container) => {
+  container.addEventListener("dragover", (e) => {
+    // "dragover" event is empty, but it is needed for "drop" to be possibe
+    e.preventDefault();
+  });
+  
+  container.addEventListener("drop", (e) => {
+    // "drop" event does the job of swapping images between containers
+    e.preventDefault();
 
-const checkboxes = document.querySelectorAll('.checkbox');
-// Attempting to update order and checkboxes
+    const draggedFrom = document.querySelector(".dragging");
+    const draggedTo = e.target;
 
-// picture.addEventListener('drop', (e) => {
-//   e.preventDefault();
-//   const draggedIndex = e.dataTransfer.getData('text/plain');
-//   const droppedIndex = index;
-//   const pictureList = Arrary.from(pictures);
-//   const newOrder = picturelist.map((picture, index) => {
-//     if (index == draggedIndex) {
-//       return pictureList[droppedIndex].getAttribute('data-step');
-//     } else if (index == droppedIndex) {
-//       return pictureList[draggedIndex].getAttribute('data-step');
-//     } else {
-//       return picture.getAttribute('data-step');
-//     }
-//     });
+    const fromContainer = draggedFrom.parentNode;
+    const toContainer = draggedTo.parentNode;
 
-//   })
+    fromContainer.removeChild(draggedFrom);
+    toContainer.removeChild(draggedTo);
 
+    fromContainer.appendChild(draggedTo);
+    toContainer.appendChild(draggedFrom);
+  });
+});
 
-//   // add drop event lister
-// function checkBoxes() {
-//   document.getElementsByClassName(.image-container.drop);
-//     if (correctOrder) == document.getElementsByClassName {
-//         document.getElementsByClassName('.cbox' checked = true) {
-//         }
+function uncheckAll() {
+  document
+    .querySelectorAll('input[type="checkbox"]')
+    .forEach((el) => (el.checked = false));
+}
 
-//     }
-// }
+document.querySelector("button").addEventListener("click", uncheckAll);
